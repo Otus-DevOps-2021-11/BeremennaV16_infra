a2f2ea7c7d75db08613d30d4605d5a064954cd0a
john <homejohn@johns-MacBook-Air.local>
2022-01-03 11:10:15 +0300

    Equal .py test fix

--- play-travis/test.py
+++ play-travis/test.py
@@ -2,8 +2,8 @@ import unittest
 
 class NumbersTest(unittest.TestCase):
 
-    def test_equal(self):
-        self.assertEqual(1 + 1, 1)
+  def test_equal(self):
+       self.assertEqual(1, 1)
 
 if __name__ == '__main__':
     unittest.main()
