f801922e2c08d61aad0532c6ed121faeee9467c4
john <homejohn@johns-MacBook-Air.local>
2022-01-23 18:32:33 +0300

    terraform-2 #9

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -3,9 +3,9 @@
 #    yandex = {
 #      source  = "yandex-cloud/yandex"
 #      version = "0.61.0"
-##  }
+#  }
 #}
-
+#}
 provider "yandex" {
   service_account_key_file = var.service_account_key_file
   cloud_id                 = var.cloud_id
@@ -18,12 +18,12 @@ module "app" {
   public_key_path = var.public_key_path
   app_disk_image = var.app_disk_image
   subnet_id = var.subnet_id
-  name            = "reddit-app-prod"
+
 }
 module "db" {
   source = "../modules/db"
   public_key_path = var.public_key_path
   db_disk_image = var.db_disk_image
   subnet_id = var.subnet_id
-  name            = "reddit-db-prod"
+
 }
