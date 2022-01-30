1d69cd27e3f04e4b271f175ca327c95754b13610
john <homejohn@johns-MacBook-Air.local>
2022-01-31 00:22:13 +0300

    ansible-2 #1

--- terraform/stage/outputs.tf
+++ terraform/stage/outputs.tf
@@ -4,3 +4,6 @@ output "external_ip_address_db" {
 output "external_ip_address_app" {
   value = module.app.external_ip_address_app
 }
+output "private_ip_address_db" {
+  value = module.db.private_ip_address_db
+}
